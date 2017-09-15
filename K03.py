2d93e138b4b6eccc18ecf83b529ba92ae56e8d2a
Karina Ionkina <kionkina@stuy.edu>
2017-09-15 01:40:51 -0400

    smol additions

--- K03.py
+++ K03.py
@@ -1,10 +1,29 @@
 """Karina Ionkina, Ibnul Jahan
 SoftDev1 pd7
-HW<n> -- <Title/Topic/Summary>
-<yyyy>-<mm>-<dd>
+HW03 -- StI/O: Divine your Destiny!
+2017-09-15
+"""
+#'r' -- read mode: file is only being read.
+#file object is created
+file = open("occupations.csv", 'r')
+
+"""
+#.read() extracts a string that contains all characters in the file
+print file.read()
+
+.readline(): read a file line by line
+print file.readline() 
+
+#.readlines() returns array with separate lines
+print file.readlines()
+
 """
 
-import csv
-With open(‘occupations.csv’, ‘r’) as csvfile:
-print csvfile
-spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+d = {}
+for str in file.readlines():
+    x = str.split(',')
+    print x
+    
+
+    
+
