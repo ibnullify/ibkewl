add036454f9b233f8f75a2f72f9691bc8e984a25
ibnullify <ijahan1@stuy.edu>
2017-09-15 13:15:22 -0400

    created dictionary

--- K03.py
+++ K03.py
@@ -7,23 +7,33 @@ HW03 -- StI/O: Divine your Destiny!
 #file object is created
 file = open("occupations.csv", 'r')
 
-"""
 #.read() extracts a string that contains all characters in the file
-print file.read()
-
+#print file.read()
+"""
 .readline(): read a file line by line
 print file.readline() 
 
 #.readlines() returns array with separate lines
 print file.readlines()
-
 """
 
+#initialize empty dictionary
 d = {}
-for str in file.readlines():
-    x = str.split(',')
-    print x
-    
+
+#file.readlines is an array with the lines of the documents
+#we parse starting at index 1
+for str in file.readlines()[1:len(file.readlines())-1]:
+    line = str.split(',')
+    #if there are commas in the job class
+    if len(line) > 2:
+        the_key = ",".join(line[:len(line)-1])
+        d[the_key] = float(line[len(line) - 1])
+    else:
+        d[line[0]] = float(line[1])
+print d
+
+
+
 
     
 
